415b2ccb0866951dba100c27f128f7967a97cc93
gaurav dhra <dhradhra@gmail.com>
2020-12-15 21:15:38 +0530

    header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import Header from  './components/Header'
+import Footer from  './components/Footer'
 function App() {
   return (
     <>
+    <Header></Header>
       <h1>Welcome Havells</h1>
+      <Footer></Footer>
     </>
   );
 }
