0c5992965547cfeb59c614ad69aafc0d9ffd5bb7
gaurav dhra <dhradhra@gmail.com>
2020-12-16 22:37:14 +0530

    screen and rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import Header from  './components/Header'
+import { Container } from 'react-bootstrap'
 import Footer from  './components/Footer'
+import HomeScreen from  './screens/HomeScreen'
 function App() {
   return (
     <>
-    <Header></Header>
-      <h1>Welcome Havells</h1>
-      <Footer></Footer>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+          <HomeScreen/>
+      </Container>
+    </main>
+      <Footer/>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/css/bootstrap.min.css';
+// import './assets/css/all-font-awesome.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
